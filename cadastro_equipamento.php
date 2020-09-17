39fd0406b205194f4119a85432340bda448b8364
Eletrica Pinho <eletricapinho@gmail.com>
2020-09-16 23:09:38 -0300

    tela de menus index e cadastro estilo

--- cadastro_equipamento.php
+++ cadastro_equipamento.php
@@ -12,7 +12,6 @@ if(isset($_POST['veiculo'])  && !empty($_POST['placa'])){
     $placa =  $_POST['placa'];
 
     if($cadastrar->cadastrarVeiculo($veiculo, $placa) == true){
-        
         header("Location: index.php");
         exit;
     }else{
@@ -30,22 +29,27 @@ if(isset($_POST['veiculo'])  && !empty($_POST['placa'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro Veiculos</title>   
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link href="assets/css/signin.css" rel="stylesheet">
 </head>
 <body>
-
-<form method="POST">
-
-
-Veiculo:<br>
-<input type="text" name="veiculo" id=""><br><br>
-
-Placa:<br>
-<input type="text" name="placa" id=""placeholder="AAA-0000" ><br><br>
-
-
-
-<input type="submit" value="Cadastrar">
+<form method="POST" class="form-signin">
+    <h2 class="form-signin-heading">Cadastro de veiculos</h2>
+    <div class="form-group">
+        <input type="text" name="veiculo" id="" placeholder="Veiculo" class="form-control">    
+    </div>
+    <div class="form-group">
+        <input type="text" name="placa" id=""placeholder="AAA-0000" class="form-control">
+    </div>
+    <div class="form-group">
+        <input type="submit"  class="btn btn-lg btn-info  btn-block" value="Cadastrar">
+    </div>
+    <div class="form-group">
+        <a href="index.php" class="btn btn-lg btn-secondary  btn-block">Voltar</a> 
+    </div>
 </form>
-    
+    <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -19,26 +19,28 @@ $veiculo = new Veiculo($pdo);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-    <script type="text/javascript" src="/assets/js/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript" src="/assets/js/jquery.min.js"></script>
-    <script type="text/javascript" src="/assets/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link href="assets/css/signin.css" rel="stylesheet">
 
 
 </head>
 <body>
-
-<a href="veiculo_uso.php">Veiculos em Uso</a><br><br>
-<a href="veiculo_disponivel.php">Veiculos Disponivel</a><br><br>
-<a href="cadastro_equipamento.php">Cadastro Veiculo</a><br><br>
-<!--<a href="#">Cadastro Operador</a><br><br> -->
-<!-- <a href="#">Veiculos  Manutenção</a><br><br> -->
-<!-- <a href="consulta.php">Consulta </a><br><br> 
-<a href="#">Consulta Operador</a><br><br> -->
-<a href="sair.php">Sair</a><br><br><br><br>
-
-
+    <div class="container">
     
-    
+        <div class="form-signin">
+            <h2 class="form-signin-heading">Tela de Menus</h2>
+            <a href="veiculo_uso.php"  class="btn btn-lg btn-info  btn-block">Veiculos em Uso</a><br><br>
+            <a href="veiculo_disponivel.php"  class="btn btn-lg btn-info  btn-block">Veiculos Disponivel</a><br><br>
+            <a href="cadastro_equipamento.php"  class="btn btn-lg btn-info  btn-block">Cadastro Veiculo</a><br><br>
+            <!--<a href="#">Cadastro Operador</a><br><br> -->
+            <!-- <a href="#">Veiculos  Manutenção</a><br><br> -->
+            <!-- <a href="consulta.php">Consulta </a><br><br> 
+            <a href="#">Consulta Operador</a><br><br> -->
+            <a href="sair.php"  class="btn btn-lg btn-danger  btn-block">Sair</a><br><br><br><br>
+        </div>
+    </div>
+    <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
