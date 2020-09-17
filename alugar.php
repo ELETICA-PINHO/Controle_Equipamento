c870c6aca81df980a4bb20034c6a765ac3f582cc
Eletrica Pinho <eletricapinho@gmail.com>
2020-09-17 15:46:25 -0300

    devolução e alugar estilo

--- alugar.php
+++ alugar.php
@@ -19,11 +19,7 @@ if(isset($_GET['id'])){
         }else{
             echo "Erros";
         }
-
-
     }
-
-
 }else{
     header("location: index.php");
     exit;
@@ -37,29 +33,39 @@ if(isset($_GET['id'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alugar</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link href="assets/css/signin.css" rel="stylesheet">
+
 </head>
 <body>
+    <div class="container">
+    <form method="POST" class="form-signin">
+        <h2 class="form-signin-heading">Alugar</h2>
+        <div class="form-group">
+            
+            <input type="text" name="operador" id="" class="form-control" placeholder="Nome Operador">
+        </div>
+        <div class="form-group">
+            <label for="dataI" >Data Inicial</label>
+            <input type="datetime-local" name="dataI" id="" class="form-control" >
+        </div>
+        <div class="form-group">
+            <label for="observacao" >Observaçãoes</label>
+            <textarea name="observacao" id="" cols="25" rows="5" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Alugar" class="btn btn-lg btn-success  btn-block">
+        </div>
+        <div class="form-group">
+            <a href="index.php"class="btn btn-lg btn-secondary  btn-block">Voltar Menu</a>
+            <a href="sair.php"class="btn btn-lg btn-danger  btn-block">Sair</a>
+        </div>
+       
+    </form>
 
-<form method="POST">
-
-Nome Operador:<br>
-<input type="text" name="operador" id=""><br><br>
-
-Data Inicial:<br>
-<input type="datetime-local" name="dataI" id=""><br><br>
-
-Observação Entrda:<br>
-<textarea name="observacao" id="" cols="25" rows="5"></textarea><br><br>
-
-<input type="submit" value="Alugar">
-
-
-</form>
-<br><br>   
-
-
-<a href="index.php">Voltar Menu</a><br><br>
-<a href="sair.php">Sair</a>
-
+    </div>
+    <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- devolucao.php
+++ devolucao.php
@@ -30,36 +30,42 @@ if(isset($_GET['id'])){
 
 
 ?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pr-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Devolução</title>
+    <title>Alugar</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link href="assets/css/signin.css" rel="stylesheet">
+
 </head>
 <body>
-<h1>Devolução</h1>
+    <div class="container">
+    <form method="POST" class="form-signin">
+        <h2 class="form-signin-heading">Devolução</h2>
+        <div class="form-group">
+            <label for="dataI" >Data Final</label>
+            <input type="datetime-local" name="dataF" id="" class="form-control" >
+        </div>
+        <div class="form-group">
+            <label for="observacao" >Observaçãoes</label>
+            <textarea name="observacao" id="" cols="25" rows="5" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Devolver" class="btn btn-lg btn-success  btn-block">
+        </div>
+        <div class="form-group">
+            <a href="index.php"class="btn btn-lg btn-secondary  btn-block">Voltar Menu</a>
+            <a href="sair.php"class="btn btn-lg btn-danger  btn-block">Sair</a>
+        </div>
+       
+    </form>
 
-<form method="POST">
-
-
-Data Final:<br>
- <input type="datetime-local" name="dataF" id=""><br><br>
-
-
- Observaçõs:<br>
- <textarea name="observacao" id="" cols="25" rows="5"></textarea><br><br>
-
- <input type="submit" value="enviar">
-
-
-</form>
-<br><br>
-    
-<a href="index.php">Voltar Menu</a><br><br>
-<a href="sair.php">Sair</a>
-
-
-
+    </div>
+    <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
